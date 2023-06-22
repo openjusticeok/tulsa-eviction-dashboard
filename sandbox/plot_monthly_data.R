ca278a2465f3710641acbe24e320149e08939176
Anthony Flores <70184250+AnthonyOKC@users.noreply.github.com>
2023-06-22 15:49:35 -0500

    Change Geography Labels
    Some changes to how the labels are displayed on the dashboard including the search bar.

--- sandbox/plot_monthly_data.R
+++ sandbox/plot_monthly_data.R
@@ -1,10 +1,67 @@
-data <-
-    read_csv(file = here("data/tulsaEvictionData.csv"))
-case_monthData <-
-    data |>
-    ojo_add_population_data() |>
-    ojo_aggregate_cases(frequency = "month")
+library(here)
+library(tidyverse)
 
-case_monthData |>
-    select(-c(population, n_per1k)) |>
-    ojo_lines(x = month_filed, y  = n)
+data <- read_csv(here("data/tulsaEvictionData.csv"))
+# This are the cases with location data ONLY. Not all of them
+data_with_locations <- read_csv(here("data/tulsa_eviction_cases.csv"))
+
+# Plots for all cases
+case_day_data <-
+    data |>
+    summarise(n = n(), .by = date)
+
+case_month_data <-
+    data |>
+    mutate(month_filed = rollback(date, roll_to_first = TRUE)) |>
+    summarise(n = n(), .by = month_filed)
+
+case_day_data |>
+    ggplot(aes(x = date, y = n)) +
+    geom_line()
+
+case_month_data |>
+    ggplot(aes(x = month_filed, y = n)) +
+    geom_line()
+
+# Plots for all cases with location data
+case_day_data_with_locations <-
+    data_with_locations |>
+    summarise(n = n(), .by = date)
+
+case_month_data_with_locations <-
+    data_with_locations |>
+    mutate(month_filed = rollback(date, roll_to_first = TRUE)) |>
+    summarise(n = n(), .by = month_filed)
+
+case_day_data_with_locations |>
+    ggplot(aes(x = date, y = n)) +
+    geom_line()
+
+case_month_data_with_locations |>
+    ggplot(aes(x = month_filed, y = n)) +
+    geom_line()
+
+# Plot for when data still has date_filed instead of date
+data <- data |>
+  filter(
+    geo_accuracy >= 0.85,
+    !is.na(lat)
+  )
+
+case_day_data <-
+    data |>
+    summarise(n = n(), .by = date_filed)
+
+case_month_data <-
+    data |>
+    mutate(month_filed = rollback(date_filed, roll_to_first = TRUE)) |>
+    summarise(n = n(), .by = month_filed)
+
+case_day_data |>
+    ggplot(aes(x = date_filed, y = n)) +
+    geom_line()
+
+case_month_data |>
+    ggplot(aes(x = month_filed, y = n)) +
+    geom_line()
+
