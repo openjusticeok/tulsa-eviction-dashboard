4dbbb083ee9ca9be08e07accb88e1ac071f07bdd
Brancen Gregory <brancengregory@gmail.com>
2023-10-24 23:37:40 -0500

    Add source calls and run automation.R

--- R/automation.R
+++ R/automation.R
@@ -1,3 +1,10 @@
+library(here)
+
+source(here("R", "get_ojodb_tulsa_eviction_data.R"))
+source(here("R", "coordinates_to_geographies.R"))
+source(here("R", "geographies_to_demographics_and_bubbles_helpers.R"))
+source(here("R", "tulsa_geographies_to_demographics_and_bubbles.R"))
+
 get_ojodb_tulsa_eviction_data() # Get Eviction Data from Jan. 2018 to Last Month
 coordinates_to_geographies() # Place Lat/Lon Case Data to Various Geographies
 tulsa_geographies_to_demographics_and_bubbles() # Apply Census Data to Each Geography
